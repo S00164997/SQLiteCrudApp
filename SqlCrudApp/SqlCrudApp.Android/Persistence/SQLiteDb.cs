38fc5d024246a9298b360334b274fb2f289fa0b5
MichaelChrystal <S00164997@mail.itsligo.ie>
2018-09-18 22:04:38 +0100

    Set Up DB

---
+++ SqlCrudApp/SqlCrudApp.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using SqlCrudApp.Persistence;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SqlCrudApp.Droid.Persistence.SQLiteDb))]
+
+namespace SqlCrudApp.Droid.Persistence
+{
+    class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
